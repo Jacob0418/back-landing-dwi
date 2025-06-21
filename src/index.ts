c6bdb927270ed1a584169beda5bc272c2fd89e9c
dapcoronado <j.coronado@dappertechnologies.com>
2025-06-20 21:19:35 -0500

    ruta endpoint

--- src/index.ts
+++ src/index.ts
@@ -19,5 +19,5 @@ app.use(express.static(path.join(__dirname, "../public")));
 
 app.listen(port, function() {
     const port = process.env.PORT || 3000;
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 })
